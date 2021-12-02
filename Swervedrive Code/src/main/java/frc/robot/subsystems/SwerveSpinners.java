3cd997c24798668333de4c62b4d9095c2f512083
asl-phoenix <r79626234@gmail.com>
2021-12-02 15:03:59 +0000

    V1. Update Swerve

--- Swervedrive Code/src/main/java/frc/robot/subsystems/SwerveSpinners.java
+++ Swervedrive Code/src/main/java/frc/robot/subsystems/SwerveSpinners.java
@@ -20,21 +20,18 @@ import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 
-/**
- * COMPETITION READY
- * 
- * Controls all wheels on frame and encoder values for auto.
- */
+
 public class SwerveSpinners extends SubsystemBase {
 
+  /** These are the variables for the SwerveSpinners subsytem. */
   public static final double MM_TO_IN = 0.0393701;
   public static final double WHEEL_TO_WHEEL_DIAMETER_INCHES = 320 * MM_TO_IN;
   public static final double WHEE7L_DIAMETER_INCHES = 4;
   public static final double MOTOR_POWER = 0.45;
 
   private WPI_TalonFX motor1, motor2, motor3, motor4;
-  //private SpeedControllerGroup spinSpeedController;
   
+  //This is the constructor for this subsytem.
   public SwerveSpinners() {
     motor1 = new WPI_TalonFX(MOTOR_PORT_1);
     motor2 = new WPI_TalonFX(MOTOR_PORT_2);
@@ -42,10 +39,10 @@ public class SwerveSpinners extends SubsystemBase {
     motor4 = new WPI_TalonFX(MOTOR_PORT_4);
   }
 
-  // MOTORS
 
-  //So this is the default command for SwerveDrive
+  //This function is the default command for the swervedrive motor spinners.
   public void spinMotors(double horizontal, double vertical, double [] currentAngles){
+    //This -1 is due to how the vertical axis works on the controller. 
     vertical *= -1;
     if (horizontal>= 0.05 && vertical >= 0.05){
       double trueSpinSpeed = (((Math.sqrt(Math.pow(horizontal, 2)+Math.pow(vertical,2)))/(Math.sqrt(2)))*MOTOR_POWER);
@@ -56,9 +53,12 @@ public class SwerveSpinners extends SubsystemBase {
     }
   }
 
+  /**
+   * This function is for getting the direction for the spin of a wheel. The current angles are useless right now.
+   * However, it may be of a use if my thinking is wrong, so I did not erase it. 
+  */
   private double getSpinDirection(double vertical, double currentAngle){
-    // here I do not multiply vertical by -1 at start: just switched > to <. 
-    if (vertical <= 0) return 1;
+    if (vertical >= 0) return 1;
     else return -1;
   }
 
