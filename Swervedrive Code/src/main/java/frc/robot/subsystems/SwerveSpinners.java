55a379c9ddca87033354501f91f0016c2fd29939
GriffinsProgrammers <90404246+GriffinsProgrammers@users.noreply.github.com>
2022-01-12 12:59:22 +0000

    hello
    this is updated and will keep a constant angular velocity when rotating. However, the issue with this is that its translational speed when doing so will not remain constant. I have not yet found a way around this. To do so, I will need to come together with a few people and derive a mathematical algorithm for each wheel.

--- Swervedrive Code/src/main/java/frc/robot/subsystems/SwerveSpinners.java
+++ Swervedrive Code/src/main/java/frc/robot/subsystems/SwerveSpinners.java
@@ -28,9 +28,9 @@ public class SwerveSpinners extends SubsystemBase {
   public static final double WHEEL_TO_WHEEL_DIAMETER_INCHES = 320 * MM_TO_IN;
   public static final double WHEEL_DIAMETER_INCHES = 4;
   // It may be more logical to use no SPEED MULTIPLIER and rather just depend on the controller input(investigate)
-  public static final double SPEED_MULTIPLIER = 0.7;
+  public static final double ROTTRANSCUT = 0.2;
+  public static final double SPEED_MULTIPLIER = 1/(Math.sqrt(2))-ROTTRANSCUT;
   public static final double ROTATION_COEFFICIENT = 0.5;
-  public static final double ROTATION_BOOST = 0; //Max value of 1-Speed Multiplier
   private WPI_TalonFX bRMotor, bLMotor, fRMotor, fLMotor;
   private SpeedControllerGroup bR, bL, fR, fL;
   
@@ -52,7 +52,7 @@ public class SwerveSpinners extends SubsystemBase {
   public void spinMotors(double horizontal, double vertical, double rotationHorizontal, double angle){
     //This -1 is due to how the vertical axis works on the controller. 
     vertical *= -1;
-    double r = (Math.pow(Math.sqrt(horizontal*horizontal + vertical*vertical),1)*SPEED_MULTIPLIER);
+    double r = (Math.pow(Math.sqrt(horizontal*horizontal + vertical*vertical),2.4)*SPEED_MULTIPLIER);
     //This makes the maximum power 1/Speed Divider. So, we can essentially add to some of the motors and
     // get it to rotate without going above 1 by accident, which would just turn to 1. (Probably)
 
@@ -102,10 +102,10 @@ public class SwerveSpinners extends SubsystemBase {
       if((angle>=315)&&(360>angle)){
         divisor = Math.toDegrees(Math.cos(angle));
       }
-      frontRightSpeed = r/divisor+ROTATION_BOOST;
-      backLeftSpeed = r/divisor+ROTATION_BOOST;
-      backRightSpeed = r/divisor+ROTATION_BOOST;
-      frontLeftSpeed = r/divisor+ROTATION_BOOST;
+      frontRightSpeed = r/divisor;
+      backLeftSpeed = r/divisor;
+      backRightSpeed = r/divisor;
+      frontLeftSpeed = r/divisor;
     }
     //This part is for no translation. There are always opposite speeds but other 2 speeds are 0 in order
     // to just rotate without translation.
